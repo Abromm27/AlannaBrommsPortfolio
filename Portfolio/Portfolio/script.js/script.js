f0696a927760d2b97fe40212ea171889bbd21e82
Abromm27 <kht6ds7tn6@privaterelay.appleid.com>
2026-04-23 17:01:43 -0400

    Add files via upload

---
+++ Portfolio/Portfolio/script.js/script.js
@@ -0,0 +1,16 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const navbar = document.getElementById("navbar");
+
+  if (!navbar) return;
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navbar.classList.add("shrink");
+  } else {
+    navbar.classList.remove("shrink");
+  }
+}
+
